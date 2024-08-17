1d44a163f0e52fc3fc7db27156454170bf41e2ec
Aleksandra Knott <aleksandraknott@gmail.com>
2024-08-17 11:17:13 +0000

    add products to home page and products page

--- item/urls.py
+++ item/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductList.as_view(), name='home'),
-    path('<slug:slug>/', views.product_detail, name='product_detail'),
+    path('', views.home, name='home'),
+    #path('<slug:slug>/', views.product_detail, name='product_detail'),
     path('<slug:slug>/edit_review/<int:review_id>', views.review_edit, name='review_edit'),
     path('<slug:slug>/delete_review/<int:review_id>', views.review_delete, name='review_delete'),
-    path('products/', views.ProductList.as_view(), name='products'),
-    path('cart/', views.Cart, name='cart'),
+    path('products/', views.store_view, name='products'),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('update-cart-item/<int:product_id>/<int:quantity>/', views.update_cart_item, name='update_cart_item'),
 ]
