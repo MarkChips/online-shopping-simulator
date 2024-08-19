a3af65fb17b81661824c599100829ca42960a507
Aleksandra Knott <aleksandraknott@gmail.com>
2024-08-19 13:01:01 +0000

    add templates for product_details and checkout pages

--- item/urls.py
+++ item/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('update-cart-item/<int:product_id>/<int:quantity>/', views.update_cart_item, name='update_cart_item'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
